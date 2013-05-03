33fd48c32502dc514da7d58f0cdec41efa80d4eb
Emile Silvis <emile@silvis.co.za>
2013-05-03 07:29:36 +0200

    Enabling rufus-scheduler

--- lib/leaderboard.rb
+++ lib/leaderboard.rb
@@ -2,26 +2,6 @@ require 'base64'
 
 class Leaderboard
 
-	def self.build_top_ten_old
-		player_discoveries = Hash.new
-		top_ten = Hash.new
-		Player.all.each do |player|
-			unless player.discoveries.size  == 4
-				player_discoveries[player.mxit_user_id] = player.discoveries.size
-			end 
-		end
-		player_discoveries.sort_by {|key,value| value}.reverse![0..9].each do |key, value|
-			user_profile = MxitUserAPI.get_user_profile(key)
-			if user_profile 
-				top_ten[user_profile['DisplayName']] = value
-			end
-		end
-
-		File.open('public/leaderboard_top_ten.json','w+') do |file|
-			file.write(top_ten.to_json)
-		end	
-	end
-
 	def self.build_top_ten
 		
 		LeaderboardEntry.all.destroy
@@ -40,10 +20,6 @@ class Leaderboard
 			end
 		end
 
-	end
-
-	def self.top_ten_old
-		JSON.load(File.open('public/leaderboard_top_ten.json','r'))
 	end
 
 	def self.top_ten
